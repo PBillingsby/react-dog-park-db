416999d9843d3d69fe56e7c4d38a05c6cfaea871
PBillingsby <philbillingsby@gmail.com>
2020-08-31 13:04:10 -0700

    Add dog_park_params, index and create in dog_parks controller

--- dog-park-db-backend/app/controllers/dog_parks_controller.rb
+++ dog-park-db-backend/app/controllers/dog_parks_controller.rb
@@ -1,5 +1,15 @@
 class DogParksController < ApplicationController
   def create
-    byebug
+    park = DogPark.create(dog_park_params)
+    render json: park
+  end
+
+  def index
+    parks = DogPark.all
+    render json: parks
+  end
+
+  def dog_park_params
+    params.require(:dogPark).permit(:name, :address, :city, :rating, :parkDescription)
   end
 end
--- dog-park-db-backend/app/models/dog_park.rb
+++ dog-park-db-backend/app/models/dog_park.rb
@@ -1,3 +1,3 @@
 class DogPark < ApplicationRecord
-
+  validates_presence_of [:name, :address, :city, :rating]
 end
